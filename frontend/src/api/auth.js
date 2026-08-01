5cd0793befa8610d1c6d36a72717764a1ef96b9a
toyb <toybamdwww@gmail.com>
2026-08-01 04:58:51 -0700

    Connect login to Render backend

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const auth = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://realstate-api-weje.onrender.com/api/",
 });
 
 export default auth;
